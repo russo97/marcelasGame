36a6469617bf02d1f0790a757f5c289a3f66a6f3
Wanderson Silva <wandersonsilva768@gmail.com>
2020-08-16 08:47:58 -0300

    arquivos iniciais, estável

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    ok
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+
+  components: {},
+
+  data() {
+    return {};
+  },
+
+  mwthods: {},
+
+  computed: {},
+
+  watch: {}
+}
+</script>
+
+<style>
+
+</style>
