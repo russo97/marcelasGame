4a333bbff52b683be0a0c61e8bd4a0f3a39355ec
Wanderson Silva <wandersonsilva768@gmail.com>
2020-10-12 04:10:12 -0300

    feat: animação no TileBlock definida

--- src/components/TileBlock/TileBlock.vue
+++ src/components/TileBlock/TileBlock.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="playingBlock">
-      {{ index }}
+  <div class="playingBlock" :class="{ showing }" @click="showing = !showing">
+      <!-- {{ index }} -->
   </div>
 </template>
 
@@ -8,13 +8,84 @@
 export default {
     name: 'TileBlock',
 
-    props: ['index']
+    props: ['index'],
+
+    data () {
+        return {
+            showing: false
+        }
+    },
+
+    watch: {
+        showing (curr) {
+            if (curr) {
+                setTimeout(() => this.showing = false, 500);
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss">
  .playingBlock {
     cursor: pointer;
+    position: relative;
     background: #b15166;
+    -webkit-tap-highlight-color: transparent;
+
+    &.showing {
+        z-index: 10;
+
+        &::before {
+            top: -50%;
+            left: -50%;
+            opacity: .3;
+            width: 200%;
+            height: 200%;
+            animation: 500ms colorchange forwards;
+        }
+
+        &::after {
+            top: 50%;
+            left: 50%;
+            opacity: .3;
+            width: 0%;
+            height: 0%;
+            animation: 800ms colorchange forwards;
+        }
+    }
+
+    &::before,
+    &::after {
+        content: "";
+        opacity: 0;
+        position: absolute;
+        pointer-events: none;
+        background-color: #fff;
+        transition: all 300ms ease;
+        border-radius: 50%;
+    }
+
+    &::before {
+        top: 50%;
+        left: 50%;
+        width: 0px;
+        height: 0px;
+    }
+
+    &::after {
+        top: -50%;
+        left: -50%;
+        width: 200%;
+        height: 200%;
+    }
+  }
+
+  @keyframes colorchange {
+      100% { opacity: 0; }
+  }
+
+  @-webkit-keyframes colorchange {
+      100% { opacity: 0; }
   }
 </style>
